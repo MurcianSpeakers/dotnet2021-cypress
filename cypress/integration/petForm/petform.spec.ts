56e649d793bef2e1bc2185f345837499baae65bd
Asun Sánchez López <aslopez@plainconcepts.com>
2021-05-09 12:59:17 +0200

    WIP: Sample with multiple assertions

--- cypress/integration/petForm/petform.spec.ts
+++ cypress/integration/petForm/petform.spec.ts
@@ -2,7 +2,7 @@
 
 describe('Pet form', () => {
     beforeEach(() => {
-      cy.visit('http://localhost:3000');
+        cy.visit('http://localhost:3000');
     });
 
     it("Should show form", () => {
@@ -13,12 +13,34 @@ describe('Pet form', () => {
         cy.get(".myapp-petform")
             .find("[data-testid='type-input']")
             .find(".myapp-pet-selector")
-            .find("input[type='radio']").should("have.length", 2);
+            .find("input[type='radio']")
+            .should("have.length", 2);
 
         cy.get(".myapp-petform")
             .find(".btn-primary")
             .should("have.text", "Add new pet");
     });
+
+    it("Should show form - multiple assertions", () => {
+        cy.get(".myapp-petform")
+            .find("[data-testid='name-input']")
+            .find("input[type='text']");
+
+        cy.get(".myapp-petform")
+            .find("[data-testid='type-input']")
+            .find(".myapp-pet-selector")
+            .find(".form-check")
+            .should("have.length", 2)
+            .and($inputs => {
+                expect($inputs.get(0)).to.have.text("Cat");
+                expect($inputs.get(1)).to.have.text("Dog");
+            });
+
+        cy.get(".myapp-petform")
+            .find(".btn-primary")
+            .should("have.text", "Add new pet");
+    });
+
 
     it("Should show list", () => {
         cy.get(".myapp-pets-list")
@@ -28,7 +50,7 @@ describe('Pet form', () => {
 
     it("Should submit a new dog", () => {
         cy.get(".myapp-petform [data-testid='name-input'] input[type='text']").type("My new dog");
-        
+
         cy.get(".myapp-pet-selector label[for='dog']").click();
 
         cy.get(".myapp-petform .btn-primary").click();
@@ -38,16 +60,52 @@ describe('Pet form', () => {
             .should("have.length", 3);
 
         cy.get(".myapp-pets-list")
-        .children("figure")
-        .eq(2)
-        .find("figcaption")
-        .should("have.text", "My new dog");
+            .children("figure")
+            .eq(2)
+            .find("figcaption")
+            .should("have.text", "My new dog");
 
         cy.get(".myapp-pets-list")
-        .children("figure")
-        .eq(2)
-        .find("img")
-        .should("have.attr", "src")
-        .should("include", "thedogapi");
+            .children("figure")
+            .eq(2)
+            .find("img")
+            .should("have.attr", "src")
+            .should("include", "thedogapi");
+    });
+
+    it("Should submit a new dog - multiple assertions", () => {
+        cy.get(".myapp-petform [data-testid='name-input'] input[type='text']").type("My new dog");
+        cy.get(".myapp-pet-selector label[for='dog']").click();
+        cy.get(".myapp-petform .btn-primary").click();
+
+        cy.get(".myapp-pets-list")
+            .find("figure")
+            .should("have.length", 3);
+
+        cy.get(".myapp-pets-list")
+            .children("figure")
+            .eq(2)
+            .find("figcaption")
+            .should("have.text", "My new dog")
+            .and("have.css", "background-color", "rgba(255, 255, 255, 0.8)");
+
+        cy.get(".myapp-pets-list")
+            .children("figure")
+            .eq(2)
+            .find("img")
+            .should("have.attr", "src")
+            .should("include", "thedogapi");
+
+        cy.get(".myapp-pets-list")
+            .children("figure")
+            .eq(2)
+            .then($el => {
+                cy.wrap($el).find("img")
+                    .should("have.attr", "src")
+                    .should("include", "thedogapi");
+                cy.wrap($el).find("figcaption")
+                    .should("have.text", "My new dog")
+                    .and("have.css", "background-color", "rgba(255, 255, 255, 0.8)");
+            });
     });
 });
